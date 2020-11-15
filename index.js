2324f33e62e156ae9435d30b70489357331e76c2
Dimitar Dimitrov <khantu12@gmail.com>
2020-11-15 13:13:16 +0100

    updated with correct filepath accessing and minor fixes

--- index.js
+++ index.js
@@ -3,22 +3,18 @@ const pug = require('pug');
 const nodeHtmlToImage = require('node-html-to-image');
 const wallpaper = require('wallpaper');
 
-const get_qod = async () => {
-  return await axios.get('https://quotes.rest/qod?language=en')
-    .then(({ data }) => data.contents.quotes[0]);
-}
-
-const main = async () => {
-  const { quote, author } = await get_qod();
-  const compiledHtml = pug.compileFile('index.pug');
-  nodeHtmlToImage({
-    output: './bg.jpg',
-    type: 'jpeg',
-    quality: 100,
-    html: compiledHtml({quote, author})
-  }).then(() => {
-    wallpaper.set('./bg.jpg');
-  });
-}
-
-main();
+axios.get('https://quotes.rest/qod?language=en')
+  .then(({ data }) => {
+    const { quote, author } = data.contents.quotes[0];
+    const compilePug = pug.compileFile(__dirname + '/index.pug');
+    const imagePath = __dirname + '/bg.jpg';
+    nodeHtmlToImage({
+      output: imagePath,
+      type: 'jpeg',
+      quality: 100,
+      html: compilePug({ quote, author })
+    }).then(() => {
+      wallpaper.set(imagePath);
+    });
+  })
+  .catch(() => console.log('API Timeout: Try again after an hour'));
